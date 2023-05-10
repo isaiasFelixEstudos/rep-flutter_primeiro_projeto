ab920e56261e91240fa1b2d925abb35be87ffa53
Isaias Félix Machado de Alcantara <isaiasofelix@gmail.com>
2023-05-10 09:17:47 -0400

    dfsdf

---
+++ rep-flutter_primeiro_projeto/lib/home_screen.dart
@@ -0,0 +1,42 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [Icon(Icons.shopping_cart)],
+        title: Text('Novo App'),
+      ),
+      drawer: Drawer(child: Text('Eu sou um DRAWER')),
+      body: const Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Nome',
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Sobrenome',
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'CPF',
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Contato',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ rep-flutter_primeiro_projeto/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:primeiro_projeto/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
